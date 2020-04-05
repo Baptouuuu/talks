e4c6f3d11266dc8d52f79ab84fa316b0587ab032
Baptiste Langlade <baptouuuu@gmail.com>
2020-04-05 12:28:46 +0200

    add adult properties

--- property based testing/code/tests/PropertyBasedTest.php
+++ property based testing/code/tests/PropertyBasedTest.php
@@ -124,4 +124,146 @@ class PropertyBasedTest extends TestCase
                 $this->assertSame(0, $citizen->age($clock));
             });
     }
+
+    public function testCitizenIsConsideredAnAdultWhenHeReachesHis18thBirthday()
+    {
+        $this
+            ->forAll(
+                PointInTime::any(),
+                Set\Composite::immutable(
+                    static function($year, $month, $day, $hour, $minute, $second, $millisecond): Composite {
+                        return new Composite(
+                            $year,
+                            $month,
+                            $day,
+                            $hour,
+                            $minute,
+                            $second,
+                            $millisecond,
+                        );
+                    },
+                    Set\Integers::between(0, 130), // small chance someone will be older than that
+                    Set\Integers::between(0, 12),
+                    Set\Integers::between(0, 30),
+                    Set\Integers::between(0, 23),
+                    Set\Integers::between(0, 59),
+                    Set\Integers::between(0, 59),
+                    Set\Integers::between(0, 999),
+                ),
+                Set\Strings::any(),
+                Set\Strings::any(),
+                Set\Strings::any(),
+            )
+            ->filter(function($birthday) {
+                // otherwise it may exceed the max year supported by PHP
+                return $birthday->year()->toInt() < 9500;
+            })
+            ->then(function($birthday, $period, $firstName, $lastName, $placeOfBirth) {
+                $now = $birthday->goForward($period->add(new Year(18)));
+                $clock = new FrozenClock($now);
+                $citizen = new Citizen(
+                    $firstName,
+                    $lastName,
+                    $birthday,
+                    $placeOfBirth,
+                );
+
+                $this->assertTrue($citizen->isAdult($clock));
+            });
+    }
+
+    public function testCitizenIsNotConsideredAnAdultWhenHeHasntReachedHis18thBirthdayYet()
+    {
+        $this
+            ->forAll(
+                PointInTime::any(),
+                Set\Composite::immutable(
+                    static function($year, $month, $day, $hour, $minute, $second, $millisecond): Composite {
+                        return new Composite(
+                            $year,
+                            $month,
+                            $day,
+                            $hour,
+                            $minute,
+                            $second,
+                            $millisecond,
+                        );
+                    },
+                    Set\Integers::between(0, 17), // can't be older than 17
+                    Set\Integers::between(0, 11),
+                    Set\Integers::between(0, 30),
+                    Set\Integers::between(0, 23),
+                    Set\Integers::between(0, 59),
+                    Set\Integers::between(0, 59),
+                    Set\Integers::between(0, 999),
+                ),
+                Set\Strings::any(),
+                Set\Strings::any(),
+                Set\Strings::any(),
+            )
+            ->filter(function($birthday) {
+                // otherwise it may exceed the max year supported by PHP
+                return $birthday->year()->toInt() < 9500;
+            })
+            ->then(function($birthday, $age, $firstName, $lastName, $placeOfBirth) {
+                $now = $birthday->goForward($age);
+                $clock = new FrozenClock($now);
+                $citizen = new Citizen(
+                    $firstName,
+                    $lastName,
+                    $birthday,
+                    $placeOfBirth,
+                );
+
+                $this->assertFalse($citizen->isAdult($clock));
+            });
+    }
+
+    public function testCitizenIsConsideredAnAdultWhenHeEmancipateNotMatterHisAge()
+    {
+        $this
+            ->forAll(
+                PointInTime::any(),
+                Set\Composite::immutable(
+                    static function($year, $month, $day, $hour, $minute, $second, $millisecond): Composite {
+                        return new Composite(
+                            $year,
+                            $month,
+                            $day,
+                            $hour,
+                            $minute,
+                            $second,
+                            $millisecond,
+                        );
+                    },
+                    Set\Integers::between(0, 130), // small chance someone will be older than that
+                    Set\Integers::between(0, 12),
+                    Set\Integers::between(0, 30),
+                    Set\Integers::between(0, 23),
+                    Set\Integers::between(0, 59),
+                    Set\Integers::between(0, 59),
+                    Set\Integers::between(0, 999),
+                ),
+                Set\Strings::any(),
+                Set\Strings::any(),
+                Set\Strings::any(),
+            )
+            ->filter(function($birthday) {
+                // otherwise it may exceed the max year supported by PHP
+                return $birthday->year()->toInt() < 9500;
+            })
+            ->then(function($birthday, $period, $firstName, $lastName, $placeOfBirth) {
+                $now = $birthday->goForward($period);
+                $clock = new FrozenClock($now);
+                $citizen = new Citizen(
+                    $firstName,
+                    $lastName,
+                    $birthday,
+                    $placeOfBirth,
+                );
+                $citizen->emancipate();
+
+                $this->assertTrue($citizen->isAdult($clock));
+            });
+    }
 }
